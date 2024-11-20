dd007e6abe9959be7894bb25d8a7226084e62494
Junkan <jrisnav1606@g.educaand.es>
2024-11-20 08:38:46 +0100

    "Apagado seguro en coche mejoras"

--- src/Coche.java
+++ src/Coche.java
@@ -28,6 +28,9 @@ public class Coche {
     public void encender() {
         System.out.println("El coche está encendido.");
     }
+    public void apagar() {
+        System.out.println("El coche se apaga de manera segura.");
+    }
     @Override
     public String toString() {
         return "Coche{" +
